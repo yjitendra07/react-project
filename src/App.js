9ef55fd96b16bd1be8f0f8de092f7eacfe990607
yjitendra07 <yjitendra.1593@gmail.com>
2021-02-18 16:02:56 +0530

    4 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import React, { Component } from "react";
 import "./style.css";
 import Person from "./Person/Person";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 class App extends Component {
-  state = {
-    persons: [
-      { name: "Max", age: 28 },
-      { name: "Monu", age: 27 },
-      { name: "Stephanie", age: 26 }
-    ],
-    otherState: "some other value"
-  };
-  //property which holds the function
-  switchNameHandler = () => {
-    // console.log("was clicked")
-    //DON"T Do this  this.state.persons[0];.name='Maximlia';
-    this.setState({
-      persons: [
-        { name: "Maxmilia", age: 28 },
-        { name: "Monu", age: 27 },
-        { name: "Stephanie", age: 20 }
-      ]
-    });
-  };
   render() {
     return (
       <div>
-        <h1>I'm devloping react</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
-    //return React.creat
   }
 }
 export default App;
